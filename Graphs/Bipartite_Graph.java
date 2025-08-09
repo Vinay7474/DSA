40e0c5312cc0e310969f5f33a53f6736adf06134
Vinay <vinaykowluri83@gmail.com>
2025-08-09 17:27:40 +0530

    graph questions  learning phase

---
+++ Graphs/Bipartite_Graph.java
@@ -0,0 +1,67 @@
+package Graphs;
+
+import java.util.HashSet;
+// https://www.geeksforgeeks.org/problems/bipartite-graph/1
+public class Bipartite_Graph {
+//     Given a Graph with V vertices (Numbered from 0 to V-1) and E edges. Check whether the graph is bipartite or not.
+
+// A bipartite graph can be colored with two colors such that no two adjacent vertices share the same color. This means we can divide the graph’s vertices into two distinct sets where:
+
+// All edges connect vertices from one set to vertices in the other set.
+// No edges exist between vertices within the same set.
+// Examples:
+
+// Input: V = 3, edges[][] = [[0, 1], [1,2]]
+// Bipartite-Graph
+// Output: true
+// Explanation: The given graph can be colored in two colors so, it is a bipartite graph.
+// Input: V = 4, edges[][] = [[0, 3], [1, 2], [3, 2], [0, 2]]
+
+
+
+
+// Output: false 
+// Explanation: The given graph cannot be colored in two colors such that color of adjacent vertices differs. 
+// Constraints:
+// 1 ≤ V ≤ 2 * 105
+// 1 ≤ edges.size() ≤ 105
+// 1 ≤ edges[i][j] ≤ 105
+
+// Expected Complexities
+// Time Complexity: O(V + E)
+// Auxiliary Space: O(V)
+// solution:---------------------------------------------------------------------------------------------------------
+public boolean isBipartite(int v, int[][] edges) {
+        // Code here
+        
+        HashSet<Integer>set1=new HashSet<>();
+        HashSet<Integer>set2=new HashSet<>();
+        for(int arr[]:edges){
+            int a=arr[0];
+            int b=arr[1];
+            if((set1.contains(a)&&set1.contains(b))||(set2.contains(a)&&set2.contains(b))){
+                return false;
+            }
+            else if (set1.contains(a)&&!set1.contains(b)){
+                set2.add(b);
+            }
+            else if (set1.contains(b)&&!set1.contains(a)){
+                set2.add(a);
+            }
+            else if (set2.contains(a)&&!set2.contains(b)){
+                set1.add(b);
+            }
+            else if (set2.contains(b)&&!set2.contains(a)){
+                set1.add(a);
+            }
+            else{
+                set1.add(a);
+                set2.add(b);
+            }
+        }
+        return true;
+        
+        
+    }
+
+}
